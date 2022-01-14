0a2194683474f0fd97115ec0c09d54c3cdf3e0c5
dayong <dayong@conflux-chain.org>
2022-01-14 15:55:58 +0800

    detail reset

--- smartContract/execute/run.js
+++ smartContract/execute/run.js
@@ -4,7 +4,8 @@ const sdk = require("js-conflux-sdk");
 const { genBatchVerifyInfo } = require("../lib/codeGen");
 const { send } = require("@openzeppelin/test-helpers");
 const fs = require("fs");
-const { cfx, iluckyDraw, sender, whiteList, drawers, registersCode, networkType } = initEnv("test")
+const { inspect } = require("util")
+const { cfx, iluckyDraw, sender, players, whiteList, drawers, registersCode, networkType } = initEnv("test")
 
 run()
 // decodeDrawEvent()
@@ -17,11 +18,11 @@ function initEnv(netType) {
 
     const cfx = new sdk.Conflux({
         url: env.url,
-        networkId: env.networkId - 10000,
+        networkId: env.networkId,
         // logger: console
     })
 
-    console.log(`env.networkId:${env.networkId}`, `content.networks[env.networkId]:`, content.networks[env.networkId])
+    // console.log(`env.networkId:${env.networkId}`, `content.networks[env.networkId]:`, content.networks[env.networkId])
 
     const iluckyDraw = cfx.Contract({ abi: content.abi, address: content.networks[env.networkId].address })
     cfx.wallet.addPrivateKey(env.from.privateKey)
@@ -29,6 +30,7 @@ function initEnv(netType) {
     return {
         cfx, iluckyDraw,
         sender: env.from.address,
+        players: env.players,
         whiteList: env.whiteList,
         drawers: env.drawers,
         registersCode: env.registersCode,
@@ -38,21 +40,23 @@ function initEnv(netType) {
 
 async function run() {
     if (networkType == "test") {
-        const verifyInfos = genBatchVerifyInfo(40 - whiteList.length);
-        console.log("verifyInfos:", verifyInfos)
+        const verifyInfos = genBatchVerifyInfo(players.length - whiteList.length);
         whiteList.push(...verifyInfos.map(i => i.hash))
         registersCode.push(...verifyInfos.map(i => i.code))
+        console.log("verifyInfos:", verifyInfos)
+        console.log("players", players)
     }
 
-    await reset()
+    await reset(true, true)
     await initialContract()
     await register()
     await setDrawer()
     await setStartTime()
 }
 
-async function reset() {
-    await iluckyDraw.reset(true, true).sendTransaction({ from: sender }).executed()
+async function reset(resetRegisterd, resetDrawPlans) {
+    await iluckyDraw.reset(resetRegisterd, resetRegisterd).sendTransaction({ from: sender }).executed()
+    console.log("reset done")
 }
 
 async function initialContract() {
@@ -61,7 +65,8 @@ async function initialContract() {
 
     // deposite to contract if test
     // if (networkType == "test") {
-    let need = (66 * 10 + 88 * 8 + 166 * 6 + 188 * 3 + 288 * 2 + 500) * 1e18
+    // let need = (66 * 10 + 88 * 8 + 166 * 6 + 188 * 3 + 288 * 2 + 500) * 1e18
+    let need = 0
     if ((await cfx.getBalance(iluckyDraw.address)) < need)
         await cfx.sendTransaction({ from: sender, to: iluckyDraw.address, gas: 30000, value: need }).executed()
     // }
@@ -87,12 +92,18 @@ async function initialContract() {
     await iluckyDraw.addDrawPlan(2, 1, 2, 0).sendTransaction({ from: sender }).executed()
     await iluckyDraw.addDrawPlan(1, 1, 1, 0).sendTransaction({ from: sender }).executed()
 
+    console.log("add standard draw plan done")
+    for(let i = 1; i <= 20; i++) {
+        await iluckyDraw.addDrawPlan(6, i, 1, 0).sendTransaction({ from: sender }).executed()
+        console.log(`add extra draw plan ${i} done`)
+    }
+
     console.log("add draw plan done")
 
     console.log("whitlist num:", await iluckyDraw.getWhiteListNum())
     console.log("draw plan num:", await iluckyDraw.getDrawPlanNum())
     console.log("next draw step:", await iluckyDraw.nextDrawStep())
-    console.log("get player:", await iluckyDraw.getPlayerByAddress(sender))
+    // console.log("get player:", await iluckyDraw.getPlayerByAddress(sender))
 }
 
 async function register() {
@@ -101,19 +112,19 @@ async function register() {
         const account = cfx.wallet.addRandom();
         let tx = cfx.sendTransaction({ from: sender, to: account.address, value: 5e17, nonce: nonce++ })
         await tx;
-        await iluckyDraw.register(registersCode[i], `我是第${i}个注册的`, `给我个iphone吧`).sendTransaction({ from: account })
+        await iluckyDraw.register(registersCode[i], `我是${players[i]}`, `我是${players[i]}, cfx to the moon`).sendTransaction({ from: account })
         if (i == registersCode.length - 1) {
             await tx.executed()
         }
         console.log(`user ${account} register ${registersCode[i]} done`)
     }
     // console.log("check is registerd:", await iluckyDraw.checkIsRegisterd(sender))
-    console.log("registered done:", registersCode.length)
+    console.log("registered done:", await iluckyDraw.getRegisters().then(i => i.length))
 }
 
 async function setDrawer() {
     const receipt = await iluckyDraw.updateDrawers(drawers).sendTransaction({ from: sender }).executed()
-    console.log("set drawer done:", receipt.outcomeStatus)
+    console.log("set drawer done:", await iluckyDraw.getDrawPlanNum())
 }
 
 async function setStartTime() {
@@ -133,7 +144,13 @@ async function decodeDrawEvent() {
 process.on('uncaughtException', (err, origin) => {
     fs.writeSync(
         process.stderr.fd,
-        `Caught exception: ${err}\n` +
-        `Exception origin: ${origin}`
+        `Caught exception: ${inspect(err, false, 5, true)}\n` +
+        `Exception origin: ${inspect(origin, false, 5, true)}`
+    );
+}).on('unhandledRejection', (reason, promise) => {
+    fs.writeSync(
+        process.stderr.fd,
+        `Unhandled rejection: ${inspect(reason, false, 5, true)}\n` +
+        `Promise: ${inspect(promise, false, 5, true)}`
     );
 });
